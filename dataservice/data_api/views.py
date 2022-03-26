87e7043b5407f533463d67ac16647882b4570c31
tawat <tawat.114.17@gmail.com>
2022-03-26 17:02:30 +0700

    update course

--- dataservice/data_api/views.py
+++ dataservice/data_api/views.py
@@ -1,12 +1,16 @@
 import requests
 import locale
 import operator
+import csv
+import codecs
 from django.http import HttpResponseNotFound
 import numpy as np
 import traceback
 import json
 from urllib.request import urlopen
 import coreapi
+from django.core.files.base import ContentFile
+from django.core.files.storage import FileSystemStorage
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.http import HttpResponse
@@ -22,6 +26,7 @@ from data_api.models import UserProfile, StudentCourseStructure, StructurePrefer
 from data_api.serializers import SetUserPassword, UserSerializer, UserDetailsSerializer, UpdateUserProfileSerializer, \
     StudentCourseStructureSerializer, StructurePreferredCourseEnrollSerializer, TransferringEquivalentCourseSerializer,\
     TransferringEquivalentCourseCreateSerializer, TransferringEquivalentCourseUpdateSerializer, ShowUserProfileSerializer
+fs = FileSystemStorage(location='tmp/')
 
 
 @api_view(['PATCH'])
@@ -96,6 +101,83 @@ class StructurePreferredCourseEnrollViewset(viewsets.ModelViewSet):
                      'course_title',
                      'course',
                      'subject', )
+
+    @action(detail=False, methods=['POST'])
+    def upload_data(self, request):
+        """Upload data from CSV"""
+        file = request.FILES["file"]
+
+        content = file.read()  # these are bytes
+        file_content = ContentFile(content)
+        file_name = fs.save(
+            "_tmp.csv", file_content
+        )
+        tmp_file = fs.path(file_name)
+
+        csv_file = open(tmp_file, errors="ignore")
+        reader = csv.reader(csv_file)
+        next(reader)
+
+        course_list = []
+        for id_, row in enumerate(reader):
+            (
+                course_code,
+                course_title,
+                credit_type,
+                credit,
+                course,
+                subject,
+                course_year,
+                description,
+
+            ) = row
+            course_list.append(
+                StructurePreferredCourseEnroll(
+                    course_code=course_code,
+                    course_title=course_title,
+                    credit_type=credit_type,
+                    credit=credit,
+                    course=course,
+                    subject=subject,
+                    course_year=course_year,
+                    description=description,
+                )
+            )
+
+        StructurePreferredCourseEnroll.objects.bulk_create(course_list)
+
+        return Response("Successfully upload the data")
+
+    @action(detail=False, methods=['POST'])
+    def upload_data_with_validation(self, request):
+        """Upload data from CSV, with validation."""
+        file = request.FILES.get("file")
+
+        reader = csv.DictReader(
+            codecs.iterdecode(file, "utf-8"), delimiter=",")
+        data = list(reader)
+
+        serializer = self.serializer_class(data=data, many=True)
+        serializer.is_valid(raise_exception=True)
+
+        course_list = []
+        for row in serializer.data:
+            course_list.append(
+                StructurePreferredCourseEnroll(
+                    course_code=row["course_code"],
+                    course_title=row["course_title"],
+                    credit_type=row["credit_type"],
+                    credit=row["credit"],
+                    course=row["course"],
+                    subject=row["subject"],
+                    course_year=row["course_year"],
+                    description=row["description"],
+                )
+            )
+
+        StructurePreferredCourseEnroll.objects.bulk_create(course_list)
+
+        return Response("Successfully upload the data")
 
 
 class TransferringEquivalentCourseViewSet(viewsets.ModelViewSet):
